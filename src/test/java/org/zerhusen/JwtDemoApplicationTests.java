9acc2eca282e28fde82d8bf7546a7c9870a43104
Glenn Van Schil <glenn.vanschil@elision.eu>
2017-05-08 11:20:24 +0200

    Remove usage of deprecated annotation

--- src/test/java/org/zerhusen/JwtDemoApplicationTests.java
+++ src/test/java/org/zerhusen/JwtDemoApplicationTests.java
@@ -3,10 +3,11 @@ package org.zerhusen;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = JwtDemoApplication.class)
+@SpringBootTest
 public class JwtDemoApplicationTests {
 
 	@Test
