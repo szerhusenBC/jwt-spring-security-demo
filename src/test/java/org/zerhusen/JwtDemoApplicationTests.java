2d82e44939c5b5dbd7d06a09ee65fb7714c3f39f
Stephan Zerhusen <s.zerhusen@byte-connect.de>
2017-06-13 11:14:59 +0200

    #28 upgraded project to Spring Boot 1.5.4

--- src/test/java/org/zerhusen/JwtDemoApplicationTests.java
+++ src/test/java/org/zerhusen/JwtDemoApplicationTests.java
@@ -2,15 +2,14 @@ package org.zerhusen;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = JwtDemoApplication.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class JwtDemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+   @Test
+   public void contextLoads() {
+   }
 }
