2d96fc5cc488984b8939c175b28b7ea8ac9c6b0c
burak sarp <sarp@air.local>
2017-07-02 16:51:23 +0300

    spring security tests added

---
+++ src/test/java/org/zerhusen/rest/AuthenticationRestControllerTest.java
@@ -0,0 +1,141 @@
+package org.zerhusen.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.zerhusen.model.security.Authority;
+import org.zerhusen.model.security.AuthorityName;
+import org.zerhusen.model.security.User;
+import org.zerhusen.security.JwtAuthenticationRequest;
+import org.zerhusen.security.JwtTokenUtil;
+import org.zerhusen.security.JwtUser;
+import org.zerhusen.security.JwtUserFactory;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthenticationRestControllerTest {
+
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @MockBean
+    private AuthenticationManager authenticationManager;
+
+    @MockBean
+    private JwtTokenUtil jwtTokenUtil;
+
+    @MockBean
+    private UserDetailsService userDetailsService;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void successfulAuthenticationWithAnonymousUser() throws Exception {
+
+        JwtAuthenticationRequest jwtAuthenticationRequest = new JwtAuthenticationRequest("user", "password");
+
+        this.mvc.perform(post("/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(jwtAuthenticationRequest)))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void successfulRefreshTokenWithUserRole() throws Exception {
+
+        Authority authority = new Authority();
+        authority.setId(0L);
+        authority.setName(AuthorityName.ROLE_USER);
+        List<Authority> authorities = Arrays.asList(authority);
+
+        User user = new User();
+        user.setUsername("username");
+        user.setAuthorities(authorities);
+        user.setEnabled(Boolean.TRUE);
+        user.setLastPasswordResetDate(new Date(System.currentTimeMillis() + 1000 * 1000));
+
+        JwtUser jwtUser = JwtUserFactory.create(user);
+
+        when(this.jwtTokenUtil.getUsernameFromToken(any())).thenReturn(user.getUsername());
+
+        when(this.userDetailsService.loadUserByUsername(eq(user.getUsername()))).thenReturn(jwtUser);
+
+        when(this.jwtTokenUtil.canTokenBeRefreshed(any(), any())).thenReturn(true);
+
+        this.mvc.perform(get("/refresh"))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void successfulRefreshTokenWithAdminRole() throws Exception {
+
+        Authority authority = new Authority();
+        authority.setId(1L);
+        authority.setName(AuthorityName.ROLE_ADMIN);
+        List<Authority> authorities = Arrays.asList(authority);
+
+        User user = new User();
+        user.setUsername("admin");
+        user.setAuthorities(authorities);
+        user.setEnabled(Boolean.TRUE);
+        user.setLastPasswordResetDate(new Date(System.currentTimeMillis() + 1000 * 1000));
+
+        JwtUser jwtUser = JwtUserFactory.create(user);
+
+        when(this.jwtTokenUtil.getUsernameFromToken(any())).thenReturn(user.getUsername());
+
+        when(this.userDetailsService.loadUserByUsername(eq(user.getUsername()))).thenReturn(jwtUser);
+
+        when(this.jwtTokenUtil.canTokenBeRefreshed(any(), any())).thenReturn(true);
+
+        this.mvc.perform(get("/refresh"))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void shouldGetUnauthorizedWithAnonymousUser() throws Exception {
+
+        this.mvc.perform(get("/refresh"))
+                .andExpect(status().isUnauthorized());
+
+    }
+
+}
+
---
+++ src/test/java/org/zerhusen/rest/MethodProtectedRestControllerTest.java
@@ -0,0 +1,59 @@
+package org.zerhusen.rest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MethodProtectedRestControllerTest {
+
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void shouldGetUnauthorizedWithoutRole() throws Exception{
+        this.mvc
+                .perform(get("/protected"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void shouldGetForbiddenWithUserRole() throws Exception{
+        this.mvc
+                .perform(get("/protected"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void shouldGetOkWithAdminRole() throws Exception{
+        this.mvc
+                .perform(get("/protected"))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+}
+
