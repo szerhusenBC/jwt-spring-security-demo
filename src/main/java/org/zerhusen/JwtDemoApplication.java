792592883181192ed65157ffefddaad10014f6b5
shellhub <shellhub.me@gmail.com>
2018-08-28 14:36:39 +0800

    UserDetailsService could not autowire.

--- src/main/java/org/zerhusen/JwtDemoApplication.java
+++ src/main/java/org/zerhusen/JwtDemoApplication.java
@@ -2,11 +2,18 @@ package org.zerhusen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.zerhusen.security.service.JwtUserDetailsService;
+
 
 @SpringBootApplication
 public class JwtDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JwtDemoApplication.class, args);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new JwtUserDetailsService();
     }
 }
