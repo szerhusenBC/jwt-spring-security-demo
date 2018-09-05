bf5ea45e7938a52c5f41d7d8ad2ae063600965d4
Nooheat <nooheat1228@gmail.com>
2018-09-05 22:59:20 +0900

    fix: Solved not imported error

--- src/main/java/org/zerhusen/security/JwtAuthorizationTokenFilter.java
+++ src/main/java/org/zerhusen/security/JwtAuthorizationTokenFilter.java
@@ -1,11 +1,5 @@
 package org.zerhusen.security;
 
-import java.io.IOException;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import io.jsonwebtoken.ExpiredJwtException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,8 +7,15 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class JwtAuthorizationTokenFilter extends OncePerRequestFilter {
 
