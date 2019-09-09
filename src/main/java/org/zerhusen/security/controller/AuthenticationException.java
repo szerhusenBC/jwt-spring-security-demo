efac70b6ba0b71f626ca8bfafcec24767da9c37e
Stephan Zerhusen <s.zerhusen@byte-connect.de>
2019-09-10 01:36:17 +0200

    fixed merge errors

--- src/main/java/org/zerhusen/security/controller/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package org.zerhusen.security.controller;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
